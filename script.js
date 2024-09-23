a4a42f7a3bad75073293912c171b275bd19065e2
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-23 10:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById("ageForm").addEventListener("submit", function(event){
+	event.preventDefault();
+	const name = document.getElementById("name").value.trim();
+	const age = parseInt(document.getElementById("age").value);
+
+	if(!name || isNaN(age)){
+		alert("inputs cannot be empty");
+		return;
+	}
+
+	let ageCheck = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age > 18){
+				resolve(`Welcome, ${name}. You can vote`);
+			}
+			else{
+				resolve(`Oh sorry ${name}. You aren't old enough`)
+			}
+		}, 4000);
+	});
+	ageCheck
+		.then(message => {
+			alert(message);
+		})
+		.catch(error => {
+			alert(error);
+		})
+})
